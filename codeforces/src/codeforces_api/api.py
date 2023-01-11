c1fa763ad782485923d7196451ba9956dc73ba8b
Pavel.Savastseika <Pavel.Savastseika@wolterskluwer.com>
2023-01-11 16:25:18 +0400

    develop codeforces api
    -- update 'get_user_info' function: return only one object instead of list.
    -- add resource files (cities.csv) to .gitignore

--- codeforces/src/codeforces_api/api.py
+++ codeforces/src/codeforces_api/api.py
@@ -50,7 +50,9 @@ class CodeForcesApi:
         return {student.nick_name: self.get_user_contests(student.nick_name) for student in students}
 
     def get_user_info(self, nick_names: str):
-        return self._get(f"user.info?handles={nick_names}")
+        users_info = self._get(f"user.info?handles={nick_names}")
+        if users_info:
+            return users_info[0]
 
     def get_users_info(self, students: List[Student]) -> dict:
         # todo: this works only if all nick_names are valid
