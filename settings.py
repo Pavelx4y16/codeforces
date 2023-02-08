e585d00480e1a0c12990f51c2078e5f38b4d21f2
Pavel.Savastseika <Pavel.Savastseika@wolterskluwer.com>
2023-02-08 19:08:30 +0400

    DataBaseURL
    --- bugfix

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 from pathlib import Path
 
-cities_path = os.environ.get('DataBaseURL') or Path("codeforces/resources") / "cities"
+cities_path = Path(os.environ.get('DataBaseURL')) if os.environ.get('DataBaseURL') else Path("codeforces/resources") / "cities"
 logger_path = Path("./logs")
